4cde6f3b3bf4d78bec185cea49fe2cd22f3990e6
mark <marklouiepanares7@gmail.com>
2026-03-11 08:39:24 +0800

    added new students

--- app.py
+++ app.py
@@ -19,3 +19,31 @@ def get_student1():
         "Year": 2,
         "Course": "BSN"
 })
+@app.route('/student3')
+def get_student2():
+    return jsonify({
+        "name": "Jermilyn",
+        "Year": 3,
+        "Course": "BSIT"
+})
+@app.route('/student4')
+def get_student3():
+    return jsonify({
+        "name": "Rene Rose",
+        "Year": 3,
+        "Course": "BSIT"
+})
+@app.route('/student5')
+def get_student4():
+    return jsonify({
+        "name": "Mae Lou",
+        "Year": 3,
+        "Course": "BSIT"
+})
+@app.route('/student6')
+def get_student5():
+    return jsonify({
+        "name": "Benjamin",
+        "Year": 3,
+        "Course": "BSIT"
+})
